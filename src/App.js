b6e56b7f3b6b72ad814b1187c7582c46342644ba
Pavanvanamala <pavankumarvanamala55@gmail.com>
2023-10-12 12:38:11 +0530

    insta changes

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Insta from './Insta'
+import Login from './Login';
+
+function App() {
+  return (
+  <>
+  <Insta/>
+  
+  </>
+  );
+}
+
+export default App;
+
---
+++ src/Login.js
@@ -0,0 +1,19 @@
+// import React from 'react'
+
+// const Login = (blog) => {
+//   const title=blog.title
+//   console.log(title)
+//   return (
+//     <div>
+//       <h2>{title}</h2>
+      
+//     </div>
+//   )
+// }
+
+// export default Login
+
+
+
+
+
